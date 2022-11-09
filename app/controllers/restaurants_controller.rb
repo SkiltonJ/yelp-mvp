b3981220171959de740ba51da67c88e8c5c1b33b
Jackson Jaensch <jackson.jaensch14@gmail.com>
2022-11-09 17:23:59 +1030

    Add create and restaurant_params methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,4 +10,15 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    redirect_to restaurant_path(@restaurant) if @restaurant.save
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
